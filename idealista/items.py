e5ad532cc9b15462312ab53c6f78f161ecae86f6
earias <eduarias@users.noreply.github.com>
2017-06-26 17:52:04 +0200

    Initial commit

---
+++ idealista/items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+
+class IdealistaItem(scrapy.Item):
+    #Matching variables of every flat to be scrapped
+    id_idealista = scrapy.Field()
+    update_date = scrapy.Field()
+    url = scrapy.Field()
+    price = scrapy.Field()
+    address = scrapy.Field()
+    sqft_m2 = scrapy.Field()
+    rooms = scrapy.Field()
+    baths = scrapy.Field()
+    discount = scrapy.Field()
+    last_updated = scrapy.Field(serializer=str)
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Installation
+
+To install scrapy:
+```bash
+sudo apt install libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
+```
+
+https://docs.scrapy.org/en/latest/intro/install.html#ubuntu-12-04-or-above
