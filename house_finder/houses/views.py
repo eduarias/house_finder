31fc8b8286c95d689bb583398a8ca701c2a194d7
earias <eduarias@users.noreply.github.com>
2017-09-14 12:55:47 +0200

    Ugly filter for houses by several criteria

--- house_finder/houses/views.py
+++ house_finder/houses/views.py
@@ -1,19 +1,36 @@
 from django.views import generic
-from django_tables2 import RequestConfig
+from django_tables2 import RequestConfig, SingleTableView
 
 from .models import House
 from .tables import HouseTable
+from .filters import HouseFilter
 
 
-class HousesListView(generic.ListView):
+class FilteredSingleTableView(SingleTableView):
+    filter_class = None
+
+    def get_table_data(self):
+        data = super(FilteredSingleTableView, self).get_table_data()
+        self.filter = self.filter_class(self.request.GET, queryset=data)
+        return self.filter.qs
+
+    def get_context_data(self, **kwargs):
+        context = super(FilteredSingleTableView, self).get_context_data(**kwargs)
+        context['filter'] = self.filter
+        return context
+
+
+class HousesListView(FilteredSingleTableView):
     model = House
     template_name = 'houses/houses_list2.html'
     context_object_name = 'houses'
     ordering = ['price']
+    filter_class = HouseFilter
+    table_class = HouseTable
 
     def get_context_data(self, **kwargs):
         context = super(HousesListView, self).get_context_data(**kwargs)
-        # context['nav_customer'] = True
+        context['nav_customer'] = True
         table = HouseTable(House.objects.order_by('price'))
         RequestConfig(self.request, paginate={'per_page': 20}).configure(table)
         context['table'] = table
