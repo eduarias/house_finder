81371e8286ce9e1c255c90e0b6070ae4756b1959
earias <eduarias@users.noreply.github.com>
2017-09-14 16:20:37 +0200

    Configure filters.

--- house_finder/houses/filters.py
+++ house_finder/houses/filters.py
@@ -1,15 +1,19 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, RangeFilter, NumberFilter, CharFilter
 
 from .models import House
 
 
 class HouseFilter(FilterSet):
+    price = RangeFilter(name='price')
+    rooms = NumberFilter(name='rooms', label='Min. rooms', lookup_expr='gte')
+    baths = NumberFilter(name='baths', label='Min. baths', lookup_expr='gte')
+    description = CharFilter(name='description', label='Description', lookup_expr='icontains')
 
     class Meta:
         model = House
-        fields = {
-            'price': ['gt', 'lt'],
-            'rooms': ['gt'],
-            'baths': ['gt'],
-            'description': ['icontains']
-        }
+        fields = [
+            'price',
+            'rooms',
+            'baths',
+            'description'
+        ]
