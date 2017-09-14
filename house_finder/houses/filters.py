5f4702af42abd2822521b3f2cbf01524672ca12e
earias <eduarias@users.noreply.github.com>
2017-09-14 17:29:50 +0200

    Filter for last few days houses

--- house_finder/houses/filters.py
+++ house_finder/houses/filters.py
@@ -1,4 +1,8 @@
-from django_filters import FilterSet, RangeFilter, NumberFilter, CharFilter
+from datetime import timedelta
+from django.utils import timezone
+
+from django_filters import FilterSet, RangeFilter, \
+    NumberFilter, CharFilter
 
 from .models import House
 
@@ -8,6 +12,7 @@ class HouseFilter(FilterSet):
     rooms = NumberFilter(name='rooms', label='Min. rooms', lookup_expr='gte')
     baths = NumberFilter(name='baths', label='Min. baths', lookup_expr='gte')
     description = CharFilter(name='description', label='Description', lookup_expr='icontains')
+    last_days = NumberFilter(name='updated_at', label='Updated last days', method='found_last_days')
 
     class Meta:
         model = House
@@ -15,5 +20,12 @@ class HouseFilter(FilterSet):
             'price',
             'rooms',
             'baths',
-            'description'
+            'description',
+            'last_days'
         ]
+
+    @staticmethod
+    def found_last_days(queryset, name, value):
+        time_threshold = timezone.now() - timedelta(days=int(value))
+        lookup = '__'.join([name, 'gte'])
+        return queryset.filter(**{lookup: time_threshold})
