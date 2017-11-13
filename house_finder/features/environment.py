8c9fabb80b8887de970a5c63c375455e2f793002
Eduardo Arias <eduarias@users.noreply.github.com>
2017-11-14 00:05:26 +0100

    Use xvfb wrapper for functional tests

--- house_finder/features/environment.py
+++ house_finder/features/environment.py
@@ -1,10 +1,18 @@
 from splinter.browser import Browser
+from xvfbwrapper import Xvfb
 
 
 def before_all(context):
-    context.browser = Browser('chrome', headless=True)
+    print("")
+    context.vdisplay = Xvfb()
+    context.vdisplay.start()
+    print("> Starting the browser")
+    context.browser = Browser('chrome')
 
 
 def after_all(context):
+    print("< Closing the browser")
+    print("")
     context.browser.quit()
     context.browser = None
+    context.vdisplay.stop()
