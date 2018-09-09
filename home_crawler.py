1c46dab6906e80e2debf1b95a940762d030e9d13
Eduardo Arias <eduarias@users.noreply.github.com>
2018-09-09 21:09:31 +0200

    add docstring

--- home_crawler.py
+++ home_crawler.py
@@ -1,3 +1,5 @@
+"""Here starts the crawling process. It runs the defined crawlers"""
+
 from time import sleep
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
