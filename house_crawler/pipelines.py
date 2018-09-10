82b9759985526d50fc2feab6d3009c28166e0943
Eduardo Arias <eduarias@users.noreply.github.com>
2018-09-10 23:50:21 +0200

    improve docstring on crawler pipeline

--- house_crawler/pipelines.py
+++ house_crawler/pipelines.py
@@ -1,3 +1,13 @@
+"""
+# Item Pipeline
+
+After an item has been scraped by a spider, it is sent to the Item Pipeline which processes it through several
+components that are executed sequentially.
+
+Each item pipeline component (sometimes referred as just “Item Pipeline”) is a Python class that implements a
+simple method. They receive an item and perform an action over it, also deciding if the item should continue
+through the pipeline or be dropped and no longer processed.
+"""
 import logging
 import re
 from abc import abstractmethod
